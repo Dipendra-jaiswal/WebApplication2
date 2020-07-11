8af24b03c8255281f169b7a9549f4c57d098f21a
dipendra-jaiswal <dipendra.jaiswal@hotmail.com>
2020-07-11 20:10:52 +0530

    Add project files.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IList<Student> students = new List<Student>();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+            students = new List<Student> { 
+            new Student{ Id = 1, Name ="Ramesh",Course="MCA",Subject="CS" },
+            new Student{ Id = 2, Name ="Suresh",Course="BTECH",Subject="IT" },
+            new Student{ Id = 3, Name ="Rajesh",Course="MCA",Subject="CS" }
+            };
+
+        }
+
+       
+        public IActionResult Index()
+        {
+            var stuList = students;
+           // var ddd = students.Where(p => p.Id == 2).First();
+           // ddd.Name = "Naresh";
+           // students[2].Name = "Nirjesh";
+           // foreach (var item in students)
+           // {
+            //    if(item.Id == 2)
+            //    {
+            //        item.Name = "Naresh";
+            //    }
+            //}            
+
+            ViewData["Hello"] = "Hello World";
+            ViewBag.Hello1 = "use view bag";
+            return View(stuList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var stdData = students.Where(p => p.Id == id).First();
+            return View(stdData);
+        }
+
+
+
+        public IActionResult Privacy(int id)
+        {
+            return View(); //"hello privacy " + id;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Course { get; set; }
+
+        public string Subject { get; set; }
+    }
+}
